e4e1eeb5f3e6cd5779549ad21a4cf89605cebe63
yuye <yeyu456@gmail.com>
2016-09-15 17:22:34 +0800

    add 2sum

---
+++ src/com/algorithms/search/TowSum.java
@@ -0,0 +1,13 @@
+package com.algorithms.search;
+
+/**
+ * Created by yuye on 2016/9/15.
+ */
+public class TowSum {
+
+    public static int twoSumR1(int[] data) {
+        int count = 0;
+        
+        return count;
+    }
+}
