ad0b37115716469145f2beef0e7679d14642d511
yuye <yeyu456@gmail.com>
2016-10-06 18:22:21 +0800

    add fraction search

--- src/com/algorithms/search/binary/FibonacciSearch.java
+++ src/com/algorithms/search/binary/FibonacciSearch.java
@@ -1,7 +1,7 @@
 package com.algorithms.search.binary;
 
 /**
- * Algorithms 4th execise 1.4.22 Fibonacci Search Implement In Ascending Order Array With Distinct Numbers
+ * Algorithms 4th exercise 1.4.22 Fibonacci Search Implement In Ascending Order Array With Distinct Numbers
  * 算法第4版 习题 1.4.22 升序排列的无重复数字数组中，基于斐波那契的二分搜索实现
  */
 public class FibonacciSearch {
@@ -13,7 +13,7 @@ public class FibonacciSearch {
      * @return (-1:查找数值不存在于数据集;其他:数值所在数据集的下标序号)
      */
     public static int search(int[] data, int num) {
-        
+
         return -1;
     }
 }
---
+++ src/com/algorithms/search/binary/FractionBinarySearch.java
@@ -0,0 +1,8 @@
+package com.algorithms.search.binary;
+
+/**
+ * Algorithms 4th exercise 1.4.23 Find Matched Fraction With Binary Search Implement
+ * 算法第4版 习题 1.4.23 排序好的数组中，使用二分搜索实现查找分数
+ */
+public class FractionBinarySearch {
+}
