3c9041f9e823ae574d89385f8e2b208e947fea3c
yuye <yeyu456@gmail.com>
2016-10-09 00:02:45 +0800

    add test

---
+++ test/com/algorithms/random/LCGTest.java
@@ -0,0 +1,18 @@
+package com.algorithms.random;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test with LCG
+ * 线性同余方法测试
+ */
+public class LCGTest {
+
+    @Test
+    public void random() throws Exception {
+
+    }
+
+}
