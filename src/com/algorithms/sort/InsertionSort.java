ede8beb0718586a11bc76c25f2b051e9ecc17537
yuye <yeyu456@gmail.com>
2016-10-12 03:21:01 +0800

    add insertion and selection sort

---
+++ src/com/algorithms/sort/InsertionSort.java
@@ -0,0 +1,17 @@
+package com.algorithms.sort;
+
+/**
+ * Algorithms 4th chapter 2.1.3 Insertion Sort
+ * 算法第4版 章节 2.1.3 插入排序
+ */
+public class InsertionSort {
+
+    public static void sort(int[] data) {
+        if (data == null || data.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < data.length; i++) {
+            
+        }
+    }
+}
